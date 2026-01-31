a71559c99a01df12fa4c8365104237bc9f00aabd
Miguel Antón <128452782+MiguelAntonGames@users.noreply.github.com>
2026-01-31 18:05:46 -0300

    UI prototype

---
+++ Assets/Scripts/UI/CustomButton.cs
@@ -0,0 +1,25 @@
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CustomButton : Button {
+
+    public override void OnPointerEnter(PointerEventData eventData) {
+        base.OnPointerEnter(eventData);
+        PlayUIHighlightSound();
+    }
+
+    public override void OnPointerClick(PointerEventData eventData) {
+        base.OnPointerClick(eventData);
+        EventSystem.current.SetSelectedGameObject(null);
+        PlayUISubmitSound();
+    }
+
+    protected virtual void PlayUIHighlightSound() {
+        //AudioManager.Instance.PlayHighlightButtonSFX();
+    }
+
+    protected virtual void PlayUISubmitSound() {
+        //AudioManager.Instance.PlayPressButtonSFX();
+    }
+
+}
