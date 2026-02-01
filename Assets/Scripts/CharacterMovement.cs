75de949188b667b2e2b3e50b82494993c7f339e6
RocketPookie <nedychini@gmail.com>
2026-02-01 11:19:49 -0300

    Camera lock when dialogue

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -9,6 +9,9 @@ public class CharacterMovement : MonoBehaviour
     private CharacterController controller;
     public Camera Camera;
     public int speed = 5;
+    
+
+    public bool dialogueStart = false;
 
     //camera
     public float mouseSensitivity = 100f;
@@ -18,34 +21,40 @@ public class CharacterMovement : MonoBehaviour
     {
        controller = GetComponent<CharacterController>();
        Cursor.lockState = CursorLockMode.Locked;
+       
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        //Player Movement
-        float x = Input.GetAxisRaw("Horizontal");
-        float z = Input.GetAxisRaw("Vertical");
-
-        Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move * speed * Time.deltaTime);
-
+        
 
 
 
         //Camera
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        if (!dialogueStart)
+        {
+            //Player Movement
+            float x = Input.GetAxisRaw("Horizontal");
+            float z = Input.GetAxisRaw("Vertical");
+
+            Vector3 move = transform.right * x + transform.forward * z;
+            controller.Move(move * speed * Time.deltaTime);
+
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
 
-        transform.Rotate(Vector3.up * mouseX);
-        xrotation -= mouseY;
-        xrotation = Mathf.Clamp(xrotation, -90f, 90f);
-        
-        Camera.transform.localRotation = Quaternion.Euler(xrotation, 0f, 0f);
+            transform.Rotate(Vector3.up * mouseX);
+            xrotation -= mouseY;
+            xrotation = Mathf.Clamp(xrotation, -90f, 90f);
 
-        //Debug.Log($"Mouse X: {mouseX} | Mouse Y: {mouseY}");
+            Camera.transform.localRotation = Quaternion.Euler(xrotation, 0f, 0f);
+
+            Debug.Log(dialogueStart);
+        }
+       
     }
 
 
