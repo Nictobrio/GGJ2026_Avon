d6252b02953daf16185c6cfd3de5b81a78440951
Fabrizio Pouget <f.pouget94@gmail.com>
2026-02-01 12:14:50 -0300

    Fix System Dialogue

--- Assets/Scripts/Dialogue System/ItemAttributes.cs
+++ Assets/Scripts/Dialogue System/ItemAttributes.cs
@@ -1,8 +1,39 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemAttributes : MonoBehaviour
+public class ItemAttributes : MonoBehaviour, ISerializationCallbackReceiver
 {
-    [SerializeField] private AnswerType type;
+    [SerializeField] Dictionary<string, Item> items;
 
-    public AnswerType Type { get => type; set => type = value; }
+    [SerializeField] List<Item> itemList;
+
+    public Dictionary<string, Item> Items { get => items; set => items = value; }
+
+    [System.Serializable]
+    public class Item
+    {
+        [SerializeField] private AnswerType type;
+
+        [SerializeField]
+        [TextArea(3, 10)]
+        private string itemName;
+        public AnswerType Type { get => type; set => type = value; }
+
+        public string ItemName { get => itemName; set => itemName = value; }
+    }
+
+    public void OnAfterDeserialize()
+    {
+        Items = new Dictionary<string, Item>();
+        int index = 0;
+
+        foreach (Item item in itemList)
+        {
+            Items.Add($"Option{index}", item);
+            index++;
+        }
+    }
+    public void OnBeforeSerialize()
+    { }
+
 }
--- Assets/Scripts/Dialogue System/UIHelper.cs
+++ Assets/Scripts/Dialogue System/UIHelper.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIHelper : MonoBehaviour
 {
     [SerializeField] private GameObject pivotSelector;
     [SerializeField] private GameObject text;
+    public AnswerType type = AnswerType.Default;
 
     public GameObject PivotSelector { get => pivotSelector; set => pivotSelector = value; }
     public GameObject Text { get => text; set => text = value; }
